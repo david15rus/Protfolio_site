c5e2006e7d04d7fdb96eecd4c5aeb24240042618
Razial <uck15@bk.ru>
2023-03-14 17:51:30 +0300

    Add new feature - send mails via Contact_Me form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,10 +1,18 @@
 import streamlit as st
+from send_email import send_email
 
 st.header('Contact Me')
 
 with st.form(key='my_form'):
     user_email = st.text_input('Your email address')
     user_text = st.text_area('Your message')
+    message = f'''\
+Subject: New email via Portfolio Website
+
+From: {user_email}
+{user_text}
+'''
     button = st.form_submit_button()
     if button:
-        pass
+        send_email(message)
+        st.info('Your message was send successfully')
--- send_email.py
+++ send_email.py
@@ -1,21 +1,17 @@
 import smtplib
 import ssl
 
-host = 'smtp.gmail.com'
-port = 465
-
+HOST = 'smtp.gmail.com'
+PORT = 465
 username = 'razzzial15@gmail.com'
 password = 'gwrvrxsyskcjfage'
-
-reciever = 'razzzial15@gmail.com'
 context = ssl.create_default_context()
 
-message = '''\
-Subject: Test mail
-Hi!
-How are you?
-Bye!
-'''
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, reciever, message)
+
+def send_email(message):
+    reciever = 'razzzial15@gmail.com'
+
+    with smtplib.SMTP_SSL(HOST, PORT, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, reciever, message)
+
